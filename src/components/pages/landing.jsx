8be91676278ec8b2d399dac40092dea46d523dac
owenkg <mcgeorgenozil@gmail.com>
2022-01-05 01:27:32 -0500

    landing page

---
+++ src/components/pages/landing.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class landing extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Welcome to the Sail Blog!</h2>
+                <p>Click <Link to="/blog">here</Link> to read some of the articles.</p>
+            </div>
+        )
+    }
+}
